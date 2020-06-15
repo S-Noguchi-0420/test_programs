93a5a4e20d705aa765f803dcad204947880b95e6
noguchi <noguchi@vri.co.jp>
2020-06-15 17:25:39 +0900

    print文を追加

--- test_python_program.py
+++ test_python_program.py
@@ -1,4 +1,11 @@
 def main():
+    msg = "1から100までの自然数nについて、\n"\
+        + "　・nが3の倍数のとき、Fizzを出力する\n"\
+        + "　・nが5の倍数のとき、Buzzを出力する\n"\
+        + "　・nが3の倍数かつ5の倍数のとき、FizzBuzzを出力する\n"\
+        + "　・nが3の倍数でも5の倍数でもないとき、nをそのまま出力する\n"
+    print(msg)
+
     for i in range(1, 101):
         if i % 15 == 0:
             print("FizzBuzz")
@@ -8,6 +15,8 @@ def main():
             print("Buzz")
         else:
             print(i)
+
+    print("\n終わりです！")
 
 if __name__ == "__main__":
     main()
