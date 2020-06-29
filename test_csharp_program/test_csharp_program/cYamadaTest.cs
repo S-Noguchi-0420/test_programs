a619d9c334bd9aa686fbbb43ee8bd16dd74d8d2c
VRI-TakeshiYamada <yamada@vri.co.jp>
2020-06-29 14:00:06 +0900

    変数追加

---
+++ test_csharp_program/test_csharp_program/cYamadaTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_csharp_program
+{
+    class cYamadaTest
+    {
+        int aaa;
+
+        public cYamadaTest()
+        {
+            aaa = 1;
+        }
+
+    }
+}
