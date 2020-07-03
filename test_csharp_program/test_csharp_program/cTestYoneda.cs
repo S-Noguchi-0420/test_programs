55ba45fa73f0d862daf3015e2273caf0ab6aceca
yoneaka <yoneaka.dayone@gmail.com>
2020-07-03 18:47:37 +0900

    Test yoneda

---
+++ test_csharp_program/test_csharp_program/cTestYoneda.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_csharp_program
+{
+ /// <summary>
+ /// Yonedaテスト
+ /// </summary>
+ class cTestYoneda
+    {
+    }
+}
